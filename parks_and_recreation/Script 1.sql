9c62a35b9cbf749232bd26d64df8ff63e37828a3
Aman Abdulla <amanabd98@gmail.com>
2024-04-10 17:02:55 +0530

    practice

---
+++ parks_and_recreation/Script 1.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+# this above statment displays Employee_demographics Table 
+
+SELECT first_name, last_name, birth_date
+FROM employee_demographics;
+# this above statment displays Employee_demographics Table with first and second name with date of birth as follows 
+# this is the syntax for this 
+
+#PEMDAS BODMAS) is absolutely relevant in SQL! It stands for the order of operations:
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+SELECT DISTINCT first_name , gender
+FROM employee_demographics
